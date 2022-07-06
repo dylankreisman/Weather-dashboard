ba6ea72737e501653a4ed1184e0220e5f7a104ff
Dr Dyl <dylkreisman@gmail.com>
2022-07-06 11:32:37 -0500

    fetching data from weather api and better class org for index

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Weather-dashboard
+
+- plug in elements according to console
+- look at document.querySelector('.tempcurrent').textContent = data2.daily[0].temp.day for reference
---
+++ script.js
@@ -0,0 +1,34 @@
+var cityName = document.querySelector(".cityname")
+var cityEl = document.getElementById('city-search')
+
+// finding results for current city info for weather
+function getCurrent(e) {
+    e.preventDefault()
+// making values for city 
+    var city = cityEl.value
+    console.log(city)
+var apiKey = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=58fb14ad5370b87fc70f19a964d8ea8c&units=imperial"
+cityName.textContent = city
+
+//fetching data
+fetch(apiKey)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+data.coord.lat+'&lon='+data.coord.lon+'&appid=58fb14ad5370b87fc70f19a964d8ea8c&units=imperial')
+    .then(res2 => res2.json())
+    //adding in current weather data attributes
+    .then(data2 => {
+        document.querySelector('.tempcurrent').textContent = data2.daily[0].temp.day
+        console.log(data2)
+
+    })
+})
+}
+
+
+var search = document.getElementById('submitBtn')
+search.addEventListener('click', getCurrent)
+
+
+//q={city name}

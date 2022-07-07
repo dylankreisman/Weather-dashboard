499329568db219228fe2a551b494c35e55f8a3c3
Dr Dyl <dylkreisman@gmail.com>
2022-07-06 21:21:34 -0500

    javascript and css aligned

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var cityName = document.querySelector("#cityname")
 var cityEl = document.getElementById('city-search')
+var cityArray = ['']
+
 
 // finding results for current city info for weather
 function getCurrent(e) {
-   // e.preventDefault()
+    // e.preventDefault()
     // making values for city 
     var city = cityEl.value
     console.log(city)
     var apiKey = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=58fb14ad5370b87fc70f19a964d8ea8c&units=imperial"
-    cityName.textContent = city
+    var currentTime = moment().format('M/DD/YYYY')
+    cityName.textContent = city + '          ' + currentTime
 
     //fetching data
     fetch(apiKey)
@@ -20,55 +23,66 @@ function getCurrent(e) {
                 .then(res2 => res2.json())
                 //adding in current weather data attributes
                 .then(data2 => {
-                     document.querySelector('#tempcurrent').textContent = "Temperature: " + data2.daily[0].temp.day + "°F"
+                    document.querySelector('#tempcurrent').textContent = "Temperature: " + data2.daily[0].temp.day + "°F"
                     console.log(data2)
                     document.querySelector('#wind').textContent = "Wind Speed: " + data2.daily[0].wind_speed
                     document.querySelector('#humidity').textContent = "Humidity: " + data2.daily[0].humidity + "%"
                     document.querySelector('#uv-index').textContent = "UV Index: " + data2.daily[0].uvi
-                    
+
                     getFuture(data2.daily)
                 })
         })
 }
-
+// getting the values of the following five days with style attributes
 function getFuture(forecast) {
-let forecastCard = document.createElement('div')
-let domDiv = document.getElementById('row')
+    let forecastCard = document.createElement('div')
+    let domDiv = document.getElementById('fiveDay')
 
-    for (i = 1; i < 5; i++) {
+    forecastCard.setAttribute('class', 'd-flex flex-row justify-content-center')
+    //creating a for loop to show the following five day forecast
+    for (i = 1; i < 6; i++) {
         let unixTime = forecast[i].dt * 1000
         let regTime = moment(unixTime).format('M/DD/YYYY')
-        
-        let date = document.createElement('div')
+
+        let date = document.createElement('ul')
         date.innerHTML = regTime
-       forecastCard.appendChild(date)
 
-        let condition = document.createElement('p')
-        condition.innerHTML = forecast[i].weather[0].description 
-        forecastCard.appendChild(condition)
+        date.setAttribute('class',
+            'card d-inline-flex flex-column m-4 p-3 list-unstyled'
+        )
 
-        let windNew = document.createElement('p')
+        forecastCard.appendChild(date)
+
+        let condition = document.createElement('li')
+        condition.innerHTML = forecast[i].weather[0].description
+        date.appendChild(condition)
+
+        let windNew = document.createElement('li')
         windNew.innerHTML = forecast[i].wind_speed + ' MPH'
-        forecastCard.appendChild(windNew)
+        date.appendChild(windNew)
 
-        let tempNew = document.createElement('p')
+        let tempNew = document.createElement('li')
         tempNew.innerHTML = forecast[i].temp.day + "°F"
-        forecastCard.appendChild(tempNew)
+        date.appendChild(tempNew)
 
-        let humidNew = document.createElement('p')
+        let humidNew = document.createElement('li')
         humidNew.innerHTML = forecast[i].humidity + '%'
-        forecastCard.appendChild(humidNew)
+        date.appendChild(humidNew)
 
     }
-  
+
     domDiv.appendChild(forecastCard)
 
 }
 
-
+//event listener at the ID button to run function once clicked
 var search = document.getElementById('submitBtn')
-search.addEventListener('click',(e) => {
-    e.preventDefault() 
-    getCurrent() })
+search.addEventListener('click', (e) => {
+    e.preventDefault()
+    getCurrent()
+})
 
+function citiesStored() {
+     
+}
 
